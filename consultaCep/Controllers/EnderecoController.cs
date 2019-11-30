2a34d373ae5619ba0679b94499b10f282b94d74d
Jean Lucas Trizotti <jeanlucastrizotti@gmail.com>
2019-11-30 13:39:02 -0300

    novo

---
+++ consultaCep/Controllers/EnderecoController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using consultaCep.DAO;
+using consultaCep.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace consultaCep.Controllers
+{
+    public class EnderecoController : Controller
+    {
+        private readonly EnderecoDAO _enderecoDAO;
+        public EnderecoController(EnderecoDAO enderecoDAO)
+        {
+            
+            _enderecoDAO = enderecoDAO;
+        }
+        public IActionResult Index()
+        {
+            ViewBag.Data = DateTime.Now;
+            return View(_enderecoDAO.Listar());
+        }
+        public IActionResult CadastrarEndereco()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult CadastrarEndereco(Endereco e)
+        {
+            //string url = @"https://api.postmon.com.br/v1/cep/" + e.Cep ;
+            string url = @"https://viacep.com.br/ws/"+e.Cep+"/json/";
+            WebClient client = new WebClient();
+            string resultado = client.DownloadString(url);
+            Endereco enderecoNovo = JsonConvert.DeserializeObject<Endereco>(resultado);
+
+            //_pedidoDAO.Cadastrar(pedido);
+            _enderecoDAO.Cadastrar(enderecoNovo);
+
+            // return View();
+            return RedirectToAction("Index");
+        }
+
+
+    }
+}
---
+++ consultaCep/DAO/EnderecoDAO.cs
@@ -0,0 +1,31 @@
+﻿using consultaCep.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace consultaCep.DAO
+{
+    public class EnderecoDAO
+    {
+        private readonly Context _context;
+        private object[] Id;
+
+        public EnderecoDAO(Context context)
+        {
+            _context = context;
+        }
+
+        public void Cadastrar(Endereco e)
+        {
+            _context.Enderecos.Add(e);
+            _context.SaveChanges();
+        }
+        public List<Endereco> Listar()
+        {
+            return _context.Enderecos.ToList();
+        }
+
+
+    }
+}
